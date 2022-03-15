886047fa0bea5128c203225fd6657a7ca9f2748c
psy <1070418190@qq.com>
2022-03-16 01:30:55 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author liang
+ * @creat 2022-03-16-1:24
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello,Git!");
+    }
+}
