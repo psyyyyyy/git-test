94df0c7b9378e7e144dcbc9f5554b2e141795783
psy <1070418190@qq.com>
2022-03-16 11:30:05 +0800

    hotfix commit1

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -7,5 +7,6 @@ package com.atguigu.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello,Git!");
+        System.out.println(1232414);
     }
 }
